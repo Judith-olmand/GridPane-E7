4ab0a7e4754d6367ecb188104bbf8dbeecd9a491
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-05-20 13:52:30 +0200

    acabado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,54 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import javafx.application.Application;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        Button norte = new Button("NORTE");
+        Button sur = new Button("SUR");
+        Button este = new Button("ESTE");
+        Button oeste = new Button("OESTE");
+        Button centro = new Button("CENTRO");
+
+        GridPane grid = new GridPane();
+        /*
+        COLUMNA 0      1        2         3
+        +---------+---------+---------+---------+
+        |  0 - 0  |  1 - 0  |  0 - 0  |  1 - 0  |   FILA 0
+        +---------+---------+---------+---------+
+        |  0 - 1  |  1 - 1  |  2 - 1  |  3 - 1  |   FILA 1
+        +---------+---------+---------+---------+
+        |  0 - 2  |  1 - 2  |  2 - 2  |  3 - 2  |   FILA 2
+        +---------+---------+---------+---------+
+        */
+
+        //COLUMNA -- FILA
+        grid.add(norte,1,0);
+        grid.add(oeste,0,1);
+        grid.add(centro,1,1);
+        grid.add(este,2,1);
+        grid.add(sur,1,2);
+
+        //Centra la cuadrícula dentro de la ventana
+        grid.setAlignment(Pos.CENTER);
+
+        //Centra los nodos horizontalmente dentro de su celda
+        GridPane.setHalignment(norte,HPos.CENTER);
+        GridPane.setHalignment(sur,HPos.CENTER);
+
+        Scene scene = new Scene(grid, 300, 200);
+        primaryStage.setTitle("Botones colocado en 'tabla', Ejercicio 7");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
